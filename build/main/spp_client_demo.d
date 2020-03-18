fb1fc930ce2564d0f17b0530d0b6ccb45acd92e7
Miroslav Tzonkov <atvservice@mail.bg>
2020-03-18 11:42:44 +0200

    Initial commit

---
+++ build/main/spp_client_demo.d
@@ -0,0 +1,234 @@
+spp_client_demo.o: \
+ /home/smooker/src/espressif/bt_smooker/main/spp_client_demo.c \
+ /home/smooker/src/espressif/esp-idf/components/driver/include/driver/uart.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_err.h \
+ /home/smooker/src/espressif/esp-idf/components/newlib/platform_include/assert.h \
+ /home/smooker/src/espressif/bt_smooker/build/include/sdkconfig.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_compiler.h \
+ /home/smooker/src/espressif/esp-idf/components/esp32/include/esp_intr_alloc.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_api.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/hal.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_context.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/tie.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/corebits.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/system.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime-frames.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/core.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtensa-versions.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/core-matmap.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/FreeRTOS.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/FreeRTOSConfig.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_config.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/projdefs.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/portable.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/deprecated_definitions.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/portmacro.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/specreg.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime-core-state.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_private/crosscore_int.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_timer/include/esp_timer.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/soc/spinlock.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/soc/cpu.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/xt_instr_macros.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/hal/cpu_hal.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/hal/cpu_types.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/src/esp32/include/hal/cpu_ll.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/cpu_caps.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/extreg.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_bit_defs.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/soc/soc_memory_layout.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/soc.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_assert.h \
+ /home/smooker/src/espressif/esp-idf/components/xtensa/include/esp_attr.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/soc/compare_set.h \
+ /home/smooker/src/espressif/esp-idf/components/heap/include/esp_heap_caps.h \
+ /home/smooker/src/espressif/esp-idf/components/heap/include/multi_heap.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/portbenchmark.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/mpu_wrappers.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_system.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_idf_version.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/semphr.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/queue.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/task.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/list.h \
+ /home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/queue.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_ringbuf/include/freertos/ringbuf.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/hal/uart_types.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/uart_caps.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/include/esp_bt.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_task.h \
+ /home/smooker/src/espressif/esp-idf/components/nvs_flash/include/nvs_flash.h \
+ /home/smooker/src/espressif/esp-idf/components/nvs_flash/include/nvs.h \
+ /home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_partition.h \
+ /home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_flash.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/hal/spi_flash_types.h \
+ /home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_types.h \
+ /home/smooker/src/espressif/esp-idf/components/soc/include/hal/esp_flash_err.h \
+ /home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_spi_flash.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_device.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gattc_api.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_main.h \
+ /home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_common_api.h \
+ /home/smooker/src/espressif/esp-idf/components/log/include/esp_log.h \
+ /home/smooker/src/espressif/esp-idf/components/log/include/esp_log_internal.h
+
+/home/smooker/src/espressif/esp-idf/components/driver/include/driver/uart.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_err.h:
+
+/home/smooker/src/espressif/esp-idf/components/newlib/platform_include/assert.h:
+
+/home/smooker/src/espressif/bt_smooker/build/include/sdkconfig.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_compiler.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp32/include/esp_intr_alloc.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_api.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/hal.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_context.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/tie.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/corebits.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/system.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime-frames.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/core.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtensa-versions.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/core-matmap.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/FreeRTOS.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/FreeRTOSConfig.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/xtensa_config.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_rom/include/esp32/rom/ets_sys.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/projdefs.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/portable.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/deprecated_definitions.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/portmacro.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/specreg.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xtensa/xtruntime-core-state.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_private/crosscore_int.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_timer/include/esp_timer.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/soc/spinlock.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/soc/cpu.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/xt_instr_macros.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/hal/cpu_hal.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/hal/cpu_types.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/src/esp32/include/hal/cpu_ll.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/cpu_caps.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/esp32/include/xtensa/config/extreg.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_bit_defs.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/soc/soc_memory_layout.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/soc.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_assert.h:
+
+/home/smooker/src/espressif/esp-idf/components/xtensa/include/esp_attr.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/soc/compare_set.h:
+
+/home/smooker/src/espressif/esp-idf/components/heap/include/esp_heap_caps.h:
+
+/home/smooker/src/espressif/esp-idf/components/heap/include/multi_heap.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/xtensa/include/freertos/portbenchmark.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/mpu_wrappers.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_system.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_idf_version.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/semphr.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/queue.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/task.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/list.h:
+
+/home/smooker/src/espressif/esp-idf/components/freertos/include/freertos/queue.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_ringbuf/include/freertos/ringbuf.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/hal/uart_types.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/soc/esp32/include/soc/uart_caps.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/include/esp_bt.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_task.h:
+
+/home/smooker/src/espressif/esp-idf/components/nvs_flash/include/nvs_flash.h:
+
+/home/smooker/src/espressif/esp-idf/components/nvs_flash/include/nvs.h:
+
+/home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_partition.h:
+
+/home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_flash.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/hal/spi_flash_types.h:
+
+/home/smooker/src/espressif/esp-idf/components/esp_common/include/esp_types.h:
+
+/home/smooker/src/espressif/esp-idf/components/soc/include/hal/esp_flash_err.h:
+
+/home/smooker/src/espressif/esp-idf/components/spi_flash/include/esp_spi_flash.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_device.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_defs.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gattc_api.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_defs.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_bt_main.h:
+
+/home/smooker/src/espressif/esp-idf/components/bt/host/bluedroid/api/include/api/esp_gatt_common_api.h:
+
+/home/smooker/src/espressif/esp-idf/components/log/include/esp_log.h:
+
+/home/smooker/src/espressif/esp-idf/components/log/include/esp_log_internal.h:
+/home/smooker/src/espressif/bt_smooker/main/./spp_client_demo.c:
---
+++ go.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make flash
+./go_ser.sh
